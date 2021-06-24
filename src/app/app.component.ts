1c4b8c6542f5eb44914baedf9f1ef262c8db2639
varshaRaj <minivarsha.a@netcon.in>
2021-06-24 14:21:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewEncapsulation, ViewChild, ElementRef, PipeTransform, Pipe, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) { }
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'embed-grafana';
+  selectRole = ['Netcon', 'Yavar'];
+  getData: boolean = false;
+  jsonData: any = [];
+  getPanels: any;
+  private _jsonURL = 'assets/_files/Home-1624505836296.json';
+  panelIdArr: any = [];
+  imageLinkArr: any = [];
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.httpClient.get(this._jsonURL).subscribe(data =>{
+      this.jsonData = data;
+      this.getPanels = this.jsonData.panels;
+    });
+  }
+
+  changeRole(event) {
+    this.panelIdArr = [];
+    for (let key of Object.keys(this.getPanels)) {
+      let panelData = this.getPanels[key];
+      if(panelData.title.includes(event.target.value)) {
+        this.panelIdArr.push(panelData.id);
+      }
+    }
+    this.getData = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, SafePipe } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
