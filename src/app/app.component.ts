15f59a2f9fc135fd05751223cb9120ffd368ccc7
varshaRaj <minivarsha.a@netcon.in>
2021-07-07 11:26:50 +0530

    cors fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, ViewEncapsulation, ViewChild, ElementRef, PipeTransform, Pipe, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import axios from 'axios';
 @Pipe({ name: 'safe' })
 export class SafePipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) { }
@@ -14,7 +15,7 @@ export class SafePipe implements PipeTransform {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'embed-grafana';
   selectRole = ['Netcon', 'Yavar'];
   getData: boolean = false;
@@ -23,23 +24,37 @@ export class AppComponent implements OnInit{
   private _jsonURL = 'assets/_files/Home-1624505836296.json';
   panelIdArr: any = [];
   imageLinkArr: any = [];
+  data;
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    this.httpClient.get(this._jsonURL).subscribe(data =>{
+    this.data = this.getGrafana();
+    this.httpClient.get(this._jsonURL).subscribe(data => {
       this.jsonData = data;
       this.getPanels = this.jsonData.panels;
     });
   }
 
   changeRole(event) {
+    console.log(this.data);
+
     this.panelIdArr = [];
     for (let key of Object.keys(this.getPanels)) {
       let panelData = this.getPanels[key];
-      if(panelData.title.includes(event.target.value)) {
+      if (panelData.title.includes(event.target.value)) {
         this.panelIdArr.push(panelData.id);
       }
     }
     this.getData = true;
   }
+
+  getGrafana() {
+    // let header = new HttpHeaders();
+   this.httpClient.get('http://localhost:4200/api/dashboards/uid/rX-QKIRnk').subscribe(res => {
+     console.log(res);
+
+   });
+
+  }
 }
+
